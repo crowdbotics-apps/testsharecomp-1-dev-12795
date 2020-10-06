9f846a600eada588e24e36df0e367ae0be1b6dff
MananSharma <manan@crowdbotics.com>
2020-10-07 05:00:55 +0530

    Update screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -1,4 +1,3 @@
-import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import {createDrawerNavigator} from 'react-navigation-drawer';
 
